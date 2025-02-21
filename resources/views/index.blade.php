cfd8b7fcba21c112275907ee9fe26040590273f5
cheryl7114 <157477132+cheryl7114@users.noreply.github.com>
2025-02-21 20:01:56 +0000

    changed home page background image, text and style

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="background-image grid grid-cols-1 m-auto">
-        <div class="flex text-gray-100 pt-10">
-            <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
-                <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
-                    Do you want to become a developer?
+    <div class="relative background-image">
+        <!-- Blurred overlay -->
+        <div class="absolute inset-0 backdrop-blur-md"></div>
+
+        <!-- Centered White Box -->
+        <div class="relative flex items-center justify-center h-full">
+            <div class="bg-white bg-opacity-50 w-3/5 h-80 p-12 rounded-md shadow-lg text-center flex flex-col justify-center">
+                <h1 class="text-gray-900 text-5xl font-bold">
+                    Love dogs?
                 </h1>
-                <a 
+                <h2 class="text-gray-700 py-3 px-6 text-3xl font-bold">
+                    You're at the right place!
+                </h2>
+                <a
                     href="/blog"
-                    class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
+                    class="mt-10 inline-block bg-pink-100 py-3 px-7 font-bold text-xl uppercase rounded">
                     Read More
                 </a>
             </div>
@@ -25,7 +32,7 @@
             <h2 class="text-3xl font-extrabold text-gray-600">
                 Struggling to be a better web developer?
             </h2>
-            
+
             <p class="py-8 text-gray-500 text-s">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus.
             </p>
@@ -34,7 +41,7 @@
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente magnam vero nostrum! Perferendis eos molestias porro vero. Vel alias.
             </p>
 
-            <a 
+            <a
                 href="/blog"
                 class="uppercase bg-blue-500 text-gray-100 text-s font-extrabold py-3 px-8 rounded-3xl">
                 Find Out More
@@ -43,7 +50,7 @@
     </div>
 
     <div class="text-center p-15 bg-black text-white">
-        <h2 class="text-2xl pb-5 text-l"> 
+        <h2 class="text-2xl pb-5 text-l">
             I'm an expert in...
         </h2>
 
@@ -86,7 +93,7 @@
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas necessitatibus dolorum error culpa laboriosam. Enim voluptas earum repudiandae consequuntur ad? Expedita labore aspernatur facilis quasi ex? Nemo hic placeat et?
                 </h3>
 
-                <a 
+                <a
                     href=""
                     class="uppercase bg-transparent border-2 border-gray-100 text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
                     Find Out More
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,16 +15,25 @@
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
-<body class="bg-gray-100 h-screen antialiased leading-none font-sans">
+<body class="bg-pink-50 h-screen antialiased leading-none font-sans">
     <div id="app">
-        <header class="bg-gray-800 py-6">
+        <header class="bg-purple-300 py-8">
             <div class="container mx-auto flex justify-between items-center px-6">
                 <div>
-                    <a href="{{ url('/') }}" class="text-lg font-semibold text-gray-100 no-underline">
+                    <a href="{{ url('/') }}" class="text-3xl font-semibold text-gray-700 no-underline flex items-center gap-2">
+                        <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 122.88 122.87">
+                            <path fill="white" d="M16.69,101.1l-0.22-0.02c-4.9-0.42-9.18-2.78-12.14-6.24c-2.97-3.48-4.63-8.07-4.27-12.97
+                            c0-0.08,0.01-0.18,0.02-0.26c0.42-4.9,2.78-9.18,6.24-12.14c3.51-3,8.15-4.66,13.1-4.27l11.88,0.96
+                            c11.62-11.62,23.23-23.24,34.85-34.86l-0.96-11.88c-0.38-4.96,1.27-9.6,4.27-13.1c3-3.5,7.33-5.87,12.27-6.25
+                            c4.96-0.38,9.6,1.27,13.1,4.27c3.5,3,5.87,7.33,6.25,12.27l0.43,5.33l2.85-0.1l0-0.01h0.14c4.88-0.1,9.34,1.73,12.66,4.82
+                            c3.36,3.14,5.53,7.55,5.7,12.51v0.09h0.01v0.14c0.12,4.88-1.73,9.34-4.82,12.66c-3.14,3.36-7.55,5.53-12.52,5.7l-15.46,0.58
+                            C79.47,68.91,68.91,79.49,58.3,90.09l-0.58,15.46c-0.18,4.96-2.34,9.38-5.7,12.51c-3.36,3.13-7.92,4.98-12.88,4.81
+                            c-4.96-0.18-9.38-2.34-12.51-5.7c-3.13-3.36-4.98-7.92-4.81-12.88l0.1-2.75L16.69,101.1L16.69,101.1L16.69,101.1z"/>
+                        </svg>
                         Pulabo
                     </a>
                 </div>
-                <nav class="space-x-4 text-gray-300 text-sm sm:text-base">
+                <nav class="space-x-4 text-gray-600 text-sm sm:text-base">
                     <a class="no-underline hover:underline" href="/">Home</a>
                     <a class="no-underline hover:underline" href="/blog">Blog</a>
                     @guest
