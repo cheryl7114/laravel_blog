fcb886a6d09e995d65fde460a4403b82c65e00cd
cheryl7114 <157477132+cheryl7114@users.noreply.github.com>
2025-03-15 19:19:57 +0000

    fixed wrong variable type in likes migration table

--- database/migrations/2025_03_15_120000_create_likes_table.php
+++ database/migrations/2025_03_15_120000_create_likes_table.php
@@ -18,7 +18,7 @@ class CreateLikesTable extends Migration
             $table->timestamps();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->unsignedBigInteger('post_id');
+            $table->unsignedInteger('post_id');
             $table->foreign('post_id')->references('id')->on('posts');
         });
     }
