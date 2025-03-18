57145c0928ee53639f1d1ef5f8d007ccae0ca093
cheryl7114 <157477132+cheryl7114@users.noreply.github.com>
2025-03-18 19:35:04 +0000

    back button styling

--- resources/views/layouts/back.blade.php
+++ resources/views/layouts/back.blade.php
@@ -1,4 +1,4 @@
-<a href="javascript:history.back()" class="mt-3 mb-8 flex items-center text-blue-500 hover:text-blue-600">
+<a href="javascript:history.back()" class="mt-3 mb-8 flex items-center text-blue-500 hover:text-blue-700">
     <svg class="w-5 h-5 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
     </svg>
