5c0d89cb7e22c955d258106a665b2ba0eca1df7a
cheryl7114 <157477132+cheryl7114@users.noreply.github.com>
2025-03-18 21:43:10 +0000

    rebase - cancelled post type

--- database/migrations/2021_02_22_174718_posts.php
+++ database/migrations/2021_02_22_174718_posts.php
@@ -4,15 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Posts extends Migration
-{
+class Posts extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('posts', function (Blueprint $table){
             $table->increments('id');
             $table->string('slug');
@@ -30,8 +28,7 @@ class Posts extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('posts');
     }
 }
